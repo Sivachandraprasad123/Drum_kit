306a7362c2af653e5d16b7cb80933e8fcce5ec00
Sivachandraprasad123 <105776191+Sivachandraprasad123@users.noreply.github.com>
2023-07-12 11:52:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,37 +23,37 @@ function makeSound(x)
 {
     if(x === "w")
     {
-      var audio = new Audio('sounds/tom-1.mp3');
+      var audio = new Audio('tom-1.mp3');
       audio.play();
     }
     else if(x === "a")
     {
-      var audio = new Audio('sounds/tom-2.mp3');
+      var audio = new Audio('tom-2.mp3');
       audio.play();
     }
     else if(x === "s")
     {
-      var audio = new Audio('sounds/tom-3.mp3');
+      var audio = new Audio('tom-3.mp3');
       audio.play();
     }
     else if(x === "d")
     {
-      var audio = new Audio('sounds/tom-4.mp3');
+      var audio = new Audio('tom-4.mp3');
       audio.play();
     }
     if(x === "j")
     {
-      var audio = new Audio('sounds/snare.mp3');
+      var audio = new Audio('snare.mp3');
       audio.play();
     }
     if(x === "k")
     {
-      var audio = new Audio('sounds/crash.mp3');
+      var audio = new Audio('crash.mp3');
       audio.play();
     }
     if(x === "l")
     {
-      var audio = new Audio('sounds/kick-bass.mp3');
+      var audio = new Audio('kick-bass.mp3');
       audio.play();
     }
 }
